8f295081d9c9cdf30bc64076355805ab938fdca1
PK <karthikp@pageuppeople.com>
2017-07-27 16:30:34 +1000

    fixing telemetry by giving permissions to cloudwatch logs and the message name

--- src/BusinessEvents.SubscriptionEngine.Core/Notifiers/TelemetryNotifier.cs
+++ src/BusinessEvents.SubscriptionEngine.Core/Notifiers/TelemetryNotifier.cs
@@ -11,7 +11,7 @@ namespace BusinessEvents.SubscriptionEngine.Core.Notifiers
             return Task.Factory.StartNew(() =>
             {
                 var telemetryService = new TelemetryService();
-                telemetryService.LogTelemetry(@event.Header.InstanceId, "business-event", message.Header.MessageType,
+                telemetryService.LogTelemetry(@event.Header.InstanceId, "business-event", $"business-event-{message.Header.MessageType}" ,
                 new { MessageHeader = message.Header, EventHeader = @event.Header });
             });
         }
