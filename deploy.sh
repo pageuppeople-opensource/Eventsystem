ea51a0de71afc7f2cb6a522afd2456573ca6d4fa
terencet <terence.tham@pageuppeople.com>
2017-08-30 08:50:27 +1000

    update aws keys

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_SECRET_ID && export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV && export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
 serverless deploy --stage v1 --region ap-southeast-2
 
