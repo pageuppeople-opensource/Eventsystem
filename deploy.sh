deac81f312e7d6857db608c3e42fae63aebfc149
terencet <terence.tham@pageuppeople.com>
2017-09-14 13:40:06 +1000

    export in a separate line and show more info on serverless deploy

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,18 @@ fi
 #deploy to DC0 on master branch
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "####### Development Deployment Starting"
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV && export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
-    serverless deploy --stage v1 --region ap-southeast-2 --data-center dc0
+    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
+    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
+    serverless deploy --stage v1 --region ap-southeast-2 --data-center dc0 -v
     exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" = "prod" ]; then
     ## Create the keys for production
     echo "####### Production Deployment Starting"
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DC2_5 && export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DC2_5
-    serverless deploy --stage v1 --region us-east-1 --data-center dc4
+    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DC2_5
+    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DC2_5
+    serverless deploy --stage v1 --region us-east-1 --data-center dc4 -v
     exit 0
 fi
 
