494c387c8345868758093e17ae7eaea2f143a87b
terencet <terence.tham@pageuppeople.com>
2017-10-02 12:30:53 +1100

    remove logging of event data and unused handler

--- src/BusinessEvents.SubscriptionEngine.Handlers/Handler.cs
+++ src/BusinessEvents.SubscriptionEngine.Handlers/Handler.cs
@@ -57,72 +57,9 @@ namespace BusinessEvents.SubscriptionEngine.Handlers
             };
         }
 
-        public async Task<APIGatewayProxyResponse> EventSnsPost(APIGatewayProxyRequest request, ILambdaContext context)
-        {
-            var logger = context.Logger;
-            logger.Log(JsonConvert.SerializeObject(request));
-
-            var snsMessage = Amazon.SimpleNotificationService.Util.Message.ParseMessage(request.Body);
-
-            logger.Log(JsonConvert.SerializeObject(snsMessage));
-
-//            if (!snsMessage.IsMessageSignatureValid())
-//            {
-//                logger.Log("Error Invalid SNS Message Signature");
-//                return new APIGatewayProxyResponse()
-//                {
-//                    StatusCode = 401
-//                };
-//            }
-
-            if (snsMessage.IsSubscriptionType)
-            {
-                try
-                {
-                    using (var client = new HttpClient())
-                    {
-                        await client.GetAsync(snsMessage.SubscribeURL);
-                        return new APIGatewayProxyResponse()
-                        {
-                            StatusCode = 200
-                        };
-                    }
-                }
-                catch (Exception ex)
-                {
-                    logger.Log($"Unable to subscribe to SNS topic {snsMessage.TopicArn}. Error: {ex}");
-                }
-            }
-
-            var kinesisClient = AwsClientFactory.CreateKinesisClient();
-            using (var memoryStream = new MemoryStream())
-            {
-                var b = Encoding.UTF8.GetBytes(snsMessage.MessageText);
-                memoryStream.Write(b, 0, b.Length);
-                memoryStream.Seek(0, SeekOrigin.Begin);
-
-                var putRecordRequest = new PutRecordRequest
-                {
-                    Data = memoryStream,
-                    PartitionKey = DateTime.UtcNow.ToString("yyyyMMddhhmmss"),
-                    StreamName = Environment.GetEnvironmentVariable("KINESIS_STREAM_NAME")
-                };
-
-                await kinesisClient.PutRecordAsync(putRecordRequest);
-            }
-
-            return new APIGatewayProxyResponse()
-            {
-                StatusCode = 200,
-                Headers = new Dictionary<string, string>() { {"Context-Type", "application/json"} },
-                Body = "OK"
-            };
-        }
-
         public async Task EventSnsHandler(SNSEvent snsEvent, ILambdaContext context)
         {
             var logger = context.Logger;
-            logger.Log(JsonConvert.SerializeObject(snsEvent));
 
             var kinesisClient = AwsClientFactory.CreateKinesisClient();
 
@@ -203,6 +140,7 @@ namespace BusinessEvents.SubscriptionEngine.Handlers
             var dynamodbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(Environment.GetEnvironmentVariable("AWS_REGION")));
 
             logger.Log($"Number of records in kinesis event {kinesisEvent.Records.Count}");
+
             foreach (var record in kinesisEvent.Records)
             {
                 using (var sr = new StreamReader(record.Kinesis.Data))
@@ -244,7 +182,7 @@ namespace BusinessEvents.SubscriptionEngine.Handlers
         {
             var logger = context.Logger;
             var serviceProcess = Container.Resolve<IServiceProcess>();
-            logger.Log(JsonConvert.SerializeObject(dynamoDbEvent));
+
             foreach (var record in dynamoDbEvent.Records)
             {
                 Event @event;
