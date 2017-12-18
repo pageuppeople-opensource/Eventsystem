b9642db065fb4ee33e2b9af2b2b5efe9f293c903
PK <karthikp@pageuppeople.com>
2017-12-18 11:56:27 +1100

    an attempt to make it legible

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=$BUILD_VERSION.$TRAVIS_BUILD_NUMBER.$TRAVIS_BRANCH
+VERSION=${BUILD_VERSION}.${TRAVIS_BUILD_NUMBER}.${TRAVIS_BRANCH}
 
 echo "####### Build version: $BUILD_VERSION"
 echo "####### Travis build version: $TRAVIS_BUILD_NUMBER"
@@ -14,16 +14,16 @@ fi
 #deploy to DC0 on master branch
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "####### Staging Deployment Starting"
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_STAGING
-    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_STAGING
+    export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_STAGING}
+    export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_STAGING}
     serverless deploy --stage v1 --region ap-southeast-2 --data-center staging -v
 fi
 
 if [ "$TRAVIS_BRANCH" = "prod" ]; then
     ## Create the keys for production
     echo "####### Production Deployment Starting"
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PRODUCTION
-    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PRODUCTION
+    export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_PRODUCTION}
+    export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_PRODUCTION}
     serverless deploy --stage v1 --region ap-southeast-2 --data-center production -v
     
     ## You can add more environments different different accounts or region like below
