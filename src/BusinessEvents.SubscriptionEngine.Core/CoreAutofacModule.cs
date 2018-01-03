8f6ccf9501fec79c228b6d27b811d2ad4e180b15
PK <karthikp@pageuppeople.com>
2018-01-03 12:18:42 +1100

    making everything singleton - don't see anything have a problem

--- src/BusinessEvents.SubscriptionEngine.Core/CoreAutofacModule.cs
+++ src/BusinessEvents.SubscriptionEngine.Core/CoreAutofacModule.cs
@@ -12,24 +12,25 @@ namespace BusinessEvents.SubscriptionEngine.Core
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
-            builder.RegisterType<EventStreamProcessor>().As<IEventStreamProcessor>().InstancePerDependency();
-            builder.RegisterType<BusinessEventStore>().As<IBusinessEventStore>().InstancePerDependency();
-            builder.RegisterType<DeadLetterService>().As<IDeadLetterService>().InstancePerDependency();
+            builder.RegisterType<EventStreamProcessor>().As<IEventStreamProcessor>().SingleInstance();
+            builder.RegisterType<BusinessEventStore>().As<IBusinessEventStore>().SingleInstance();
+            builder.RegisterType<DeadLetterService>().As<IDeadLetterService>().SingleInstance();
 
             // Subscription engine dependencies
-            builder.RegisterType<DataStreamProcessor>().As<IDataStreamProcessor>().InstancePerDependency();
+            builder.RegisterType<DataStreamProcessor>().As<IDataStreamProcessor>().SingleInstance();
 
-            builder.RegisterType<SubscriptionProcessor>().As<ISubscriptionProcessor>().InstancePerDependency();
+            builder.RegisterType<SubscriptionProcessor>().As<ISubscriptionProcessor>().SingleInstance();
             builder.RegisterType<SubscriptionsManager>().As<ISubscriptionsManager>().SingleInstance();
-            builder.RegisterType<SubscriberErrorService>().As<ISubscriberErrorService>().InstancePerDependency();
+            builder.RegisterType<SubscriberErrorService>().As<ISubscriberErrorService>().SingleInstance();
            
-            builder.RegisterType<AuthenticationModule>().As<IAuthenticationModule>().InstancePerDependency();
+            builder.RegisterType<AuthenticationModule>().As<IAuthenticationModule>().SingleInstance();
 
-            builder.RegisterType<TelemetryNotifier>().Keyed<INotifier>(SubscriptionType.Telemetry);
-            builder.RegisterType<SlackNotifier>().Keyed<INotifier>(SubscriptionType.Slack);
-            builder.RegisterType<WebhookNotifier>().Keyed<INotifier>(SubscriptionType.Webhook);
-            builder.RegisterType<AuthenticatedWebhookNotifier>().Keyed<INotifier>(SubscriptionType.AuthenticatedWebhook);
-            builder.RegisterType<LambdaNotifier>().Keyed<INotifier>(SubscriptionType.Lambda);
+            builder.RegisterType<TelemetryNotifier>().Keyed<INotifier>(SubscriptionType.Telemetry).SingleInstance();
+            builder.RegisterType<SlackNotifier>().Keyed<INotifier>(SubscriptionType.Slack).SingleInstance();
+            
+            builder.RegisterType<WebhookNotifier>().Keyed<INotifier>(SubscriptionType.Webhook).SingleInstance();
+            builder.RegisterType<AuthenticatedWebhookNotifier>().Keyed<INotifier>(SubscriptionType.AuthenticatedWebhook).SingleInstance();
+            builder.RegisterType<LambdaNotifier>().Keyed<INotifier>(SubscriptionType.Lambda).SingleInstance();
         }
     }
 }
