3519e3151311bb940c46c5e5442e8a0869eb1a3c
PK <karthikp@pageuppeople.com>
2018-01-03 12:20:24 +1100

    making telemetry service really singleton - this should avoid telemetry failure mostly

--- BusinessEvents.SubscriptionEngine/Notifiers/TelemetryNotifier.cs
+++ BusinessEvents.SubscriptionEngine/Notifiers/TelemetryNotifier.cs
@@ -7,12 +7,18 @@ namespace BusinessEvents.SubscriptionEngine.Notifiers
 {
     public class TelemetryNotifier : INotifier
     {
+        private readonly TelemetryService telemetryService;
+
+        public TelemetryNotifier()
+        {
+            telemetryService = new TelemetryService();
+        }
+        
         public Task Notify(Subscription subscriber, Event @event)
         {
             return Task.Factory.StartNew(() =>
             {
                 Console.WriteLine($"MessageId: {@event.Message.Header.MessageId} Event: {@event.Message.Header.MessageType} Subscriber: {subscriber.Type}:{subscriber.Endpoint}");
-                var telemetryService = new TelemetryService();
                 telemetryService.LogTelemetry(@event.Header.InstanceId, "business-event", @event.Message.Header.MessageType ,
                 new { MessageHeader = @event.Message.Header, EventHeader = @event.Header });
             });
